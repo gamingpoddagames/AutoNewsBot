13c6ab6e13570e4904713a62fcaddddc1f5c1423
CoreFlow <gamingpoddagames@gmail.com>
2026-07-27 22:19:33 +0530

    Update voice.py

--- voice.py
+++ voice.py
@@ -1,1 +1,58 @@
+import os
+from gtts import gTTS
 
+from utils import clean_text
+
+VOICE_LANG = "si"
+
+
+def create_voice(script, output_file):
+
+    script = clean_text(script)
+
+    if not script:
+        return False
+
+    try:
+
+        os.makedirs(
+            os.path.dirname(output_file),
+            exist_ok=True
+        )
+
+        tts = gTTS(
+            text=script,
+            lang=VOICE_LANG,
+            slow=False
+        )
+
+        tts.save(output_file)
+
+        return True
+
+    except Exception as e:
+
+        print("Voice Error :", e)
+
+        return False
+      def test_voice():
+
+    text = """
+    ලෝකයේ නවතම පුවත් සඳහා අප සමඟ රැඳී සිටින්න.
+    """
+
+    ok = create_voice(
+        text,
+        "output/test.mp3"
+    )
+
+    if ok:
+        print("Voice Created")
+
+    else:
+        print("Failed")
+
+
+if __name__ == "__main__":
+
+    test_voice()
