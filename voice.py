ebc989c8fbeb3afde6a35331471f972e4b7a974d
CoreFlow <gamingpoddagames@gmail.com>
2026-07-27 23:42:51 +0530

    Update voice.py

--- voice.py
+++ voice.py
@@ -1,24 +1,18 @@
 import os
 from gtts import gTTS
-
 from utils import clean_text
 
 VOICE_LANG = "si"
 
 
 def create_voice(script, output_file):
-
     script = clean_text(script)
 
     if not script:
         return False
 
     try:
-
-        os.makedirs(
-            os.path.dirname(output_file),
-            exist_ok=True
-        )
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
         tts = gTTS(
             text=script,
@@ -27,32 +21,23 @@ def create_voice(script, output_file):
         )
 
         tts.save(output_file)
-
         return True
 
     except Exception as e:
-
-        print("Voice Error :", e)
-
+        print("Voice Error:", e)
         return False
-      def test_voice():
 
-    text = """
-    ලෝකයේ නවතම පුවත් සඳහා අප සමඟ රැඳී සිටින්න.
-    """
 
-    ok = create_voice(
-        text,
-        "output/test.mp3"
-    )
+def test_voice():
+    text = "ලෝකයේ නවතම පුවත් සඳහා අප සමඟ රැඳී සිටින්න."
+
+    ok = create_voice(text, "output/test.mp3")
 
     if ok:
         print("Voice Created")
-
     else:
-        print("Failed")
+        print("Voice Failed")
 
 
 if __name__ == "__main__":
-
     test_voice()
